97880ccadddd2773c49ff7c9538f2e50f34f8f32
Reza Abbasi <rz.abs.shi@gmail.com>
2018-06-10 11:49:39 +0430

    get_url and get_token utils added

---
+++ web_decouple/__init__.py
@@ -0,0 +1,1 @@
+from .services import get_url, get_token
---
+++ web_decouple/services.py
@@ -0,0 +1,25 @@
+from decouple import config
+from urllib.parse import urlencode
+
+
+def get_url(service, url_name, query_params: dict=None):
+    service_code = get_service_code(service)
+
+    service_base_url = config(service_code)
+
+    url_code = service_code + '/' + url_name
+    url = service_base_url + config(url_code)
+
+    if query_params:
+        url += '?' + urlencode(query_params)
+
+    return url
+
+
+def get_token(service):
+    token_code = get_service_code(service) + '.token'
+    return config(token_code)
+
+
+def get_service_code(service):
+    return 'srv.' + service
